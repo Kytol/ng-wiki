4ae60c6d880a2387601a00fd2d4eca331055c4fe
Janne <jannematiaskytola@gmail.com>
2022-01-02 18:05:41 +0200

    wikipedia api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { WikiService } from './wiki.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-wiki';
+  searchTerm:any;
+
+  results:any = [];
+
+  constructor(private wiki: WikiService){}
+
+
+  onSubmit(){
+    console.log('this.searchTerm: ', this.searchTerm);
+
+    this.wiki.search(this.searchTerm).subscribe((res:any)=>{
+      console.log(res)
+    })
+  }
+
 }
